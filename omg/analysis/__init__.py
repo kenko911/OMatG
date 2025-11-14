c9dd41471a635c7084f8875bb5453829d565e969
Maya Martirossyan <mm9084@nyu.edu>
2025-11-13 22:06:36 -0500

    fixing analysis init

--- omg/analysis/__init__.py
+++ omg/analysis/__init__.py
@@ -1,3 +1,3 @@
 from .analysis import (get_bonds, get_coordination_numbers, get_coordination_numbers_species, get_cov,
-                       get_space_group, get_volume_frac, match_rmsds)
+                       get_space_group, get_volume_frac, match_rmsds, match_rate_and_rmsd_corr)
 from .valid_atoms import ValidAtoms
