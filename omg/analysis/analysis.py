fixed multiprocessing memory issue in METRe computation, updates to trainer and match_rmsds

omg/analysis/analysis.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 from omg.analysis.valid_atoms import ValidAtoms
 from omg.globals import MAX_ATOM_NUM
 from omg.utils import StandardScaler
+from itertools import repeat
 
 
 # Suppress spglib warnings.
@@ -522,7 +523,7 @@ def _get_match_and_rmsd(atoms_one: ValidAtoms, atoms_two: ValidAtoms, ltol: floa
     return None
 
 
-def _get_match_and_rmsd_sequence(atoms_one: ValidAtoms, sequence_atoms_two: Sequence[ValidAtoms], ltol: float,
+def _get_match_and_rmsd_sequence(atoms_one: ValidAtoms, sequence_atoms_two: List[Tuple[int, ValidAtoms]], ltol: float,
                                  stol: float, angle_tol: float,
                                  check_reduced: bool) -> Optional[List[Tuple[float, int]]]:
     """
@@ -546,8 +547,8 @@ def _get_match_and_rmsd_sequence(atoms_one: ValidAtoms, sequence_atoms_two: Sequ
         First structure.
     :type atoms_one: ValidAtoms
     :param sequence_atoms_two:
-        Sequence of structures.
-    :type sequence_atoms_two: Sequence[ValidAtoms]
+        List of tuples each containing the index and the second structure.
+    :type sequence_atoms_two: List[Tuple[int, ValidAtoms]]
     :param ltol:
         Fractional length tolerance for pymatgen's StructureMatcher.
     :type ltol: float
@@ -568,8 +569,12 @@ def _get_match_and_rmsd_sequence(atoms_one: ValidAtoms, sequence_atoms_two: Sequ
         structure.
     :rtype: Optional[List[Tuple[float, int]]]
     """
+    if not sequence_atoms_two:
+        return None
+
     rmsds = []
-    for index, atoms_two in enumerate(sequence_atoms_two):
+
+    for index, atoms_two in sequence_atoms_two:
         res = _get_match_and_rmsd(atoms_one, atoms_two, ltol, stol, angle_tol, check_reduced)
         if res is not None:
             rmsds.append((res, index))
@@ -582,7 +587,7 @@ def _get_match_and_rmsd_sequence(atoms_one: ValidAtoms, sequence_atoms_two: Sequ
 def match_rmsds(atoms_list: Sequence[ValidAtoms], ref_list: Sequence[ValidAtoms], ltol: float = 0.2,
                 stol: float = 0.3, angle_tol: float = 5.0, number_cpus: Optional[int] = None,
                 check_reduced: bool = True,
-                enable_progress_bar: bool = True) -> Tuple[list[Optional[float]], list[Optional[float]]]:
+                enable_progress_bar: bool = True) -> Tuple[float, float, float, float, List[Optional[float]], List[Optional[float]], float, float]:
     """
     Match the structures in the first sequence of validated atoms with the structures at the same index in the second
     sequence of validated atoms and return the root-mean-square displacements between the matching structures.
@@ -633,8 +638,9 @@ def match_rmsds(atoms_list: Sequence[ValidAtoms], ref_list: Sequence[ValidAtoms]
     :type enable_progress_bar: bool
 
     :return:
-        (List of root-mean-square displacements, List of valid root-mean-square displacements).
-    :rtype: Tuple[list[Optional[float]], list[Optional[float]]]
+        Tuple of match rate, mean RMSD, valid match rate, mean valid RMSD, List of root-mean-square displacements, List of valid root-mean-square displacements,
+        corrected RMSD, valid corrected RMSD.
+    :rtype: Tuple[float, float, float, float, List[Optional[float]], List[Optional[float]], float, float]
 
     :raises ValueError:
         If the number of structures in the first list is larger than the number of structures in the reference list.
@@ -667,7 +673,25 @@ def match_rmsds(atoms_list: Sequence[ValidAtoms], ref_list: Sequence[ValidAtoms]
     valid_res = [r if atoms.valid and ref_atoms.valid else None
                  for r, atoms, ref_atoms in zip(res, atoms_list, ref_list)]
 
-    return res, valid_res
+    match_count = sum(rmsd is not None for rmsd in res)
+    filtered_rmsds = [rmsd for rmsd in res if rmsd is not None]
+    mean_rmsd = np.mean(filtered_rmsds)
+
+    valid_match_count = sum(rmsd is not None for rmsd in valid_res)
+    filtered_valid_rmsds = [rmsd for rmsd in valid_res if rmsd is not None]
+    valid_mean_rmsd = np.mean(filtered_valid_rmsds)
+
+    # counting corrected RMSE in which non-matched structures are penalized
+    corr_rmsds = [rmsd if rmsd is not None else stol for rmsd in res]
+    corr_valid_rmsds = [rmsd if rmsd is not None else stol for rmsd in valid_res]
+    corr_rmsd = np.mean(corr_rmsds)
+    valid_corr_rmsd = np.mean(corr_valid_rmsds)
+
+    return (match_count / len(atoms_list), float(mean_rmsd),
+            valid_match_count / len(atoms_list), float(valid_mean_rmsd),
+            res, valid_res,
+            float(corr_rmsd),
+            float(valid_corr_rmsd))
 
 def match_rate_and_rmsd_corr(atoms_list: Sequence[ValidAtoms], ref_list: Sequence[ValidAtoms], ltol: float = 0.2,
                 stol: float = 0.3, angle_tol: float = 5.0, number_cpus: Optional[int] = None,
@@ -745,14 +769,24 @@ def match_rate_and_rmsd_corr(atoms_list: Sequence[ValidAtoms], ref_list: Sequenc
     cpu_count = number_cpus if number_cpus is not None else os.cpu_count()
     
     # We cannot use lambda functions so we use (partial) global functions instead.
-    match_func = partial(_get_match_and_rmsd_sequence, sequence_atoms_two=ref_list, ltol=ltol, stol=stol,
+    match_func = partial(_get_match_and_rmsd_sequence, ltol=ltol, stol=stol,
                             angle_tol=angle_tol, check_reduced=check_reduced)
+
+    def correct_ref_list():
+        # Iterable with relevant reference atoms
+        for atoms in atoms_list:
+            # Find relevant compositions and global indices in ref_list and yield them in a list
+            yield [(idx, ref_atoms) for idx, ref_atoms in enumerate(ref_list) if _element_check(atoms.atoms, ref_atoms.atoms, check_reduced=True)]
+
     if cpu_count > 1:
-        res = process_map(match_func, atoms_list, desc="Computing match rate and RMSD",
+        ref_iter = correct_ref_list()
+        res = process_map(match_func, atoms_list, ref_iter, desc="Computing METRe rate and RMSD",
                             chunksize=max(min(len(atoms_list) // cpu_count, 100), 1), max_workers=cpu_count,
                             disable=not enable_progress_bar)
     else:
-        res = list(map(match_func, atoms_list))
+        full_ref_iter = list(enumerate(ref_list))
+        res = list(map(match_func, tqdm(atoms_list, desc="Computing METRe rate and RMSD", disable=not enable_progress_bar),
+                       repeat(full_ref_iter)))
     assert len(res) == len(atoms_list)
 
     rmsds_ref = np.full((len(ref_list),), np.nan, dtype=np.float32)

omg/omg_lightning.py
@@ -7,7 +7,7 @@ import lightning
 import numpy as np
 from scipy.stats import wasserstein_distance
 import torch
-from omg.analysis import get_coordination_numbers, get_cov, match_rmsds, ValidAtoms
+from omg.analysis import get_coordination_numbers, get_cov, match_rate_and_rmsd_corr, ValidAtoms
 from omg.datamodule import OMGData
 from omg.globals import SMALL_TIME, BIG_TIME
 from omg.model.model import Model
@@ -351,15 +351,12 @@ class OMGLightning(lightning.LightningModule):
             ref_valid_atoms = ValidAtoms.get_valid_atoms(self.reference_atoms, desc="Validating reference structures",
                                                          skip_validation=True, number_cpus=1)
 
-            rmsds, valid_rmsds = match_rmsds(gen_valid_atoms, ref_valid_atoms, ltol=0.3, stol=0.5, angle_tol=10.0,
+            match_rate, mean_rmsd, _, _, _, _, corr_rmsd, _ = match_rate_and_rmsd_corr(gen_valid_atoms, ref_valid_atoms, ltol=0.3, stol=0.5, angle_tol=10.0,
                                              number_cpus=self.number_cpus, enable_progress_bar=True)
-            match_count = sum(rmsd is not None for rmsd in rmsds)
-            match_rate = match_count / len(gen_valid_atoms)
-            filtered_rmsds = [rmsd for rmsd in rmsds if rmsd is not None]
-            mean_rmsd = np.mean(filtered_rmsds)
 
-            self.log("match_rate", match_rate, sync_dist=True)
+            self.log("match_rate", float(match_rate), sync_dist=True)
             self.log("mean_rmsd", float(mean_rmsd), sync_dist=True)
+            self.log("corr_rmsd", float(corr_rmsd), sync_dist=True)
         elif self._validation_metric == self.ValidationMetric.DNG_EVAL:
             assert len(self.generated_atoms) == len(self.reference_atoms)
             gen_valid_atoms = ValidAtoms.get_valid_atoms(self.generated_atoms, desc="Validating generated structures",

omg/omg_trainer.py
@@ -22,7 +22,7 @@ from omg.sampler.minimum_permutation_distance import correct_for_minimum_permuta
 from omg.si.corrector import PeriodicBoundaryConditionsCorrector
 from omg.utils import convert_ase_atoms_to_data, xyz_reader
 from omg.analysis import (get_coordination_numbers, get_coordination_numbers_species, get_cov, get_space_group,
-                          get_volume_frac, match_rmsds, ValidAtoms)
+                          get_volume_frac, match_rmsds, match_rate_and_rmsd_corr, ValidAtoms)
 
 
 class OMGTrainer(Trainer):
@@ -698,6 +698,21 @@ class OMGTrainer(Trainer):
             Defaults to 10.0.
             This argument can be optionally set on the command line.
         :type angle_tol: float
+        :param METRe:
+            Defaults to True.
+            If True, the match-everyone-to-reference (METRe) match rate is computed instead of standard one-to-one match rate.
+            If there are no polymorphs, METRe will give the same match rate as the standard one-to-one match rate.
+            Should be avoided if there are duplicate structures in the dataset.
+            This argument can be optionally set on the command line.
+        :type METRe: bool
+        :param cRMSE:
+            Defaults to True.
+            If True, the corrected RMSE is returned in which stol is assigned as the RMSE
+            for non-matching structures.
+            Can be used with either choice of match rate (METRe or standard).
+            If all structures match, cRMSE will be the same as RMSE.
+            This argument can be optionally set on the command line.
+        :type cRMSE: bool
         :param number_cpus:
             Number of CPUs to use for multiprocessing. If None, use os.cpu_count().
             Defaults to None.
@@ -774,88 +789,68 @@ class OMGTrainer(Trainer):
             print(f"Rate of valid structures in generated dataset: "
                   f"{100 * sum(va.valid for va in gen_valid_atoms) / len(gen_valid_atoms)}%.")
 
-        if not skip_match and not METRe:
-            rmsds, valid_rmsds = match_rmsds(
-                gen_valid_atoms, ref_valid_atoms, ltol=ltol, stol=stol, angle_tol=angle_tol, number_cpus=number_cpus,
-                check_reduced=check_reduced)
-            assert len(rmsds) == len(valid_rmsds) == len(gen_valid_atoms)
-
-            match_count = sum(rmsd is not None for rmsd in rmsds)
-            match_rate = match_count / len(gen_valid_atoms)
-            filtered_rmsds = [rmsd for rmsd in rmsds if rmsd is not None]
-            mean_rmsd = np.mean(filtered_rmsds)
-
-            valid_match_count = sum(rmsd is not None for rmsd in valid_rmsds)
-            valid_match_rate = valid_match_count / len(gen_valid_atoms)
-            filtered_valid_rmsds = [rmsd for rmsd in valid_rmsds if rmsd is not None]
-            mean_valid_rmsd = np.mean(filtered_valid_rmsds)
-
-            print(f"The match rate between all generated structures and the prediction dataset is "
-                  f"{100.0 * match_rate}%.")
-            print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), between all generated "
-                  f"structures and the prediction dataset is {mean_rmsd}.")
-            print()
-            print(f"The match rate between valid generated structures and the valid prediction dataset is "
-                  f"{100.0 * valid_match_rate}%.")
-            print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), between valid generated "
-                  f"structures and the valid prediction dataset is {mean_valid_rmsd}.")
-
-            with open(result_name, "w") as f:
-                json.dump({
-                    "match_rate": match_rate,
-                    "mean_RMSE": mean_rmsd,
-                    "valid_match_rate": valid_match_rate,
-                    "valid_mean_RMSE": mean_valid_rmsd
-                }, f, indent=4)
-
-        elif not skip_match and METRe:
-            fmr, frmsd, vmr, vrmsd, rmsds, val_rmsds, corr_rmsd, vcorr_rmsd = match_rate_and_rmsd_corr(
-                gen_valid_atoms, ref_valid_atoms, ltol=ltol, stol=stol, angle_tol=angle_tol, number_cpus=number_cpus,
-                check_reduced=check_reduced)
-
-            assert len(rmsds) == len(val_rmsds) == len(gen_valid_atoms)
-
-            print(f"The match everyone to reference (METRe) rate for all generated structures is {100.0 * fmr}%.")
-            print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), with respect to all reference structures "
-                f"and all generated structures is {frmsd}.")
-            print()
-            print(f"The match everyone to reference (METRe) rate for all valid generated structures is {100.0 * vmr}%.")
-            print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), with respect to all reference structures "
-                f"and all valid generated structures is {vrmsd}.")
-
-            with open(result_name, "w") as f:
-                json.dump({
-                    "METRe_rate": fmr,
-                    "mean_RMSE": frmsd,
-                    "valid_METRe_rate": vmr,
-                    "valid_mean_RMSE": vrmsd,
-                }, f, indent=4)
-        
-        if cRMSE:
-            if not METRe: # need to compute cRMSE for non-METRe case
-                corr_rmsds = [rmsd if rmsd is not None else stol for rmsd in rmsds]
-                corr_valid_rmsds = [rmsd if rmsd is not None else stol for rmsd in valid_rmsds]
-                corr_rmsd = np.mean(corr_rmsds)
-                vcorr_rmsd = np.mean(corr_valid_rmsds)
-            
-            print(f"The corrected average root-mean-square distance, normalized by (V / N) ** (1/3) and penalizing non-matching structures "
-                f"with stol={stol} between all generated structures and the full dataset is {corr_rmsd}.")
-            print(f"The corrected average root-mean-square distance, normalized by (V / N) ** (1/3) and penalizing non-matching structures "
-                f"with stol={stol} between all valid generated structures and the valid dataset is {vcorr_rmsd}.")
-            
-            with open(result_name, "r") as f:
-                data = json.load(f)
-            data.update({
-                "cRMSE": corr_rmsd,
-                "valid_cRMSE": vcorr_rmsd,
-            })
-            with open(result_name, "w") as f:
-                json.dump(data, f, indent=4)
-
         if not skip_match:
-            if METRe:
+            if not METRe:
+                fmr, frmsd, vmr, vrmsd, rmsds, val_rmsds, corr_rmsd, vcorr_rmsd = match_rmsds(
+                    gen_valid_atoms, ref_valid_atoms, ltol=ltol, stol=stol, angle_tol=angle_tol, number_cpus=number_cpus,
+                    check_reduced=check_reduced)
+                match_type = "match_rate"
+                filtered_rmsds = [rmsd for rmsd in rmsds if rmsd is not None]
+                filtered_valid_rmsds = [rmsd for rmsd in val_rmsds if rmsd is not None]
+
+                assert len(rmsds) == len(val_rmsds) == len(gen_valid_atoms)
+
+                print(f"The match rate between all generated structures and the prediction dataset is "
+                        f"{100.0 * fmr}%.")
+                print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), between all generated "
+                        f"structures and the prediction dataset is {frmsd}.")
+                print()
+                print(f"The match rate between valid generated structures and the valid prediction dataset is "
+                        f"{100.0 * vmr}%.")
+                print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), between valid generated "
+                        f"structures and the valid prediction dataset is {vrmsd}.")
+
+            elif METRe:
+                fmr, frmsd, vmr, vrmsd, rmsds, val_rmsds, corr_rmsd, vcorr_rmsd = match_rate_and_rmsd_corr(
+                    gen_valid_atoms, ref_valid_atoms, ltol=ltol, stol=stol, angle_tol=angle_tol, number_cpus=number_cpus,
+                    check_reduced=check_reduced)
+                match_type = "METRe_rate"
                 filtered_rmsds = rmsds[~np.isnan(rmsds)]
                 filtered_valid_rmsds = val_rmsds[~np.isnan(val_rmsds)]
+
+                assert len(rmsds) == len(val_rmsds) == len(gen_valid_atoms)
+
+                print(f"The match everyone to reference (METRe) rate for all generated structures is {100.0 * fmr}%.")
+                print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), with respect to all reference structures "
+                    f"and all generated structures is {frmsd}.")
+                print()
+                print(f"The match everyone to reference (METRe) rate for all valid generated structures is {100.0 * vmr}%.")
+                print(f"The mean root-mean-square distance, normalized by (V / N) ** (1/3), with respect to all reference structures "
+                    f"and all valid generated structures is {vrmsd}.")
+
+            with open(result_name, "w") as f:
+                json.dump({
+                    "{}".format(match_type): fmr,
+                    "mean_RMSE": frmsd,
+                    "valid_{}".format(match_type): vmr,
+                    "valid_mean_RMSE": vrmsd
+                }, f, indent=4)
+        
+            if cRMSE:
+                print(f"The corrected average root-mean-square distance, normalized by (V / N) ** (1/3) and penalizing non-matching structures "
+                    f"with stol={stol} between all generated structures and the full dataset is {corr_rmsd}.")
+                print(f"The corrected average root-mean-square distance, normalized by (V / N) ** (1/3) and penalizing non-matching structures "
+                    f"with stol={stol} between all valid generated structures and the valid dataset is {vcorr_rmsd}.")
+                
+                with open(result_name, "r") as f:
+                    data = json.load(f)
+                data.update({
+                    "cRMSE": corr_rmsd,
+                    "valid_cRMSE": vcorr_rmsd,
+                })
+                with open(result_name, "w") as f:
+                    json.dump(data, f, indent=4)
+                
             plt.figure()
             bandwidth = np.std(filtered_rmsds) * len(filtered_rmsds) ** (-1 / 5)  # Scott's rule.
             filtered_rmsds = np.array(filtered_rmsds)[:, np.newaxis]